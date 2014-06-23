5277a355fba8488fb88c864042d60403607e01ea
Sean Smith <seansmith1020@gmail.com>
2014-06-23 09:40:28 -0600

    prints sum of the elements

--- numbers.rb
+++ numbers.rb
@@ -1,7 +1,14 @@
 numbers = [20, 33, 54, 21, 87, 32, 99, 10]
 
-puts "Number of elements: \n"
+puts "\nNumber of elements:\n"
 print numbers.size
-puts "\nLargest number: \n"
+puts "\nLargest number:\n"
 print numbers.sort.last
+
+sum = 0
+numbers.each do |num|
+  sum += num
+end
+puts "\nSum of the elements:\n"
+print sum
 
