71bc103f9c7e520af454a30057213955381c4373
Sean Smith <seansmith1020@gmail.com>
2014-06-23 09:49:52 -0600

    more readable when printing in terminal

--- numbers.rb
+++ numbers.rb
@@ -1,14 +1,12 @@
 numbers = [20, 33, 54, 21, 87, 32, 99, 10]
 
-puts "\nNumber of elements:\n"
-print numbers.size
-puts "\nLargest number:\n"
-print numbers.sort.last
+puts "\nNumber of elements: #{numbers.size}\n"
+puts "\nLargest number: #{numbers.sort.last}\n"
 
 sum = 0
 numbers.each do |num|
   sum += num
 end
-puts "\nSum of the elements:\n"
-print sum
+puts "\nSum of the elements: #{sum}\n\n"
+
 
